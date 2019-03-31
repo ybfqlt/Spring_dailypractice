fde12053c44d247e30f2c09f40d22c5891c56737
ybfqlt <823030735@qq.com>
2019-03-31 22:07:15 +0800

    更改了knights.xml 让诗人主动歌颂骑士

--- src/main/java/com/springinaction/knights/BraveKnight.java
+++ src/main/java/com/springinaction/knights/BraveKnight.java
@@ -2,15 +2,10 @@ package com.springinaction.knights;
 
 public class BraveKnight  implements Knight{
     private Quest quest;
-/*    private Minstrel minstrel;*/
-
     public BraveKnight(Quest quest){
         this.quest = quest;
-        /*this.minstrel = minstrel;*/
     }
     public void embarkOnQuest() {
-        /*minstrel.singBeforeQuest();*/
         quest.embark();
-       /* minstrel.singAfterQuest();*/
     }
 }
