0aa4cb67ad6c19d7935c02d9edc3e83dab32881c
ybfqlt <823030735@qq.com>
2019-04-10 18:11:53 +0800

    04-10使用环绕通知实现Audience

--- src/main/java/com4/concert1/Audience.java
+++ src/main/java/com4/concert1/Audience.java
@@ -1,9 +1,32 @@
 package com4.concert1;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
+//使用环绕通知重新实现Audience切面
 
+@Aspect
+public class Audience{
+    @Pointcut("execution(* *com4.concert1.Performance.perform(..))")
+    public void performance(){}
+
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint jp){
+        try{
+            System.out.println("关闭手机");
+            System.out.println("入座");
+            jp.proceed();
+            System.out.println("好! 鼓掌! 鼓掌! 鼓掌!");
+        }catch(Throwable e){
+            System.out.println("退票!");
+        }
+    }
+}
+
+/*
+
+//通过@Pointcut注解声明频繁使用的切点表达式
 @Aspect
 public class Audience{
     @Pointcut("execution(* *com4.concert1.Performance.perform(..))")
@@ -29,6 +52,7 @@ public class Audience{
         System.out.println("要求退票!");
     }
 }
+*/
 
 
 
