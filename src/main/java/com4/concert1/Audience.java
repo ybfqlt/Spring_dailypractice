c14afa23cd42a1bc36a38c00911d6c52575e6510
ybfqlt <823030735@qq.com>
2019-04-10 18:01:48 +0800

    04-10使用@Pointcut

--- src/main/java/com4/concert1/Audience.java
+++ src/main/java/com4/concert1/Audience.java
@@ -1,11 +1,38 @@
 package com4.concert1;
 
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 
+
+@Aspect
+public class Audience{
+    @Pointcut("execution(* *com4.concert1.Performance.perform(..))")
+    public void performance(){}//Point签名
+
+    @Before("performance()")
+    public void silenceCellPhones(){
+        System.out.println("关掉手机");
+    }
+
+    @Before("performance()")
+    public void takeSeats(){
+        System.out.println("入座");
+    }
+
+    @AfterReturning("performance()")
+    public void applause(){
+        System.out.println("好! 鼓掌!　鼓掌!　鼓掌!");
+    }
+
+    @AfterThrowing("performance()")
+    public void demandRefund(){
+        System.out.println("要求退票!");
+    }
+}
+
+
+
+/*
 @Aspect
 public class Audience {
     @Before("execution(* com4.concert1.Performance.perform(..))")
@@ -25,3 +52,4 @@ public class Audience {
         System.out.println("要求退票!");
     }
 }
+*/
