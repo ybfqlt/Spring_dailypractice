534f7bf3ee8da3b638363af2d2103a225909832b
ybfqlt <823030735@qq.com>
2019-04-12 17:44:07 +0800

    04-12在xml中通过切面引入新功能

--- src/test/java/com4/xmlconcert2/SingerMain.java
+++ src/test/java/com4/xmlconcert2/SingerMain.java
@@ -1,7 +1,14 @@
 package com4.xmlconcert2;
 
+import com4.concert2.Performance;
+import com4.concert2.Singer;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SingerMain {
     public static void main(String[] args) {
-
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/xxconcert2xml.xml");
+        Performance per = (Performance)context.getBean("dancer");
+        per.perform();
+        ((Singer)per).sing();
     }
 }
