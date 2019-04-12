1405ed9acbe59b4687f10929e0634ee8b6a6e243
ybfqlt <823030735@qq.com>
2019-04-12 17:22:15 +0800

    04-11在xml中通过切面引入新功能

--- src/test/java/com4/chuacanconcert/TrackCounterMain.java
+++ src/test/java/com4/chuacanconcert/TrackCounterMain.java
@@ -10,7 +10,6 @@ public class TrackCounterMain {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/track.xml");
-
         CompactDisc cd = (CompactDisc) context.getBean("cd");
 
         TrackCounter counter = (TrackCounter)context.getBean("trackCounter");
---
+++ src/test/java/com4/xmlconcert2/SingerMain.java
@@ -0,0 +1,7 @@
+package com4.xmlconcert2;
+
+public class SingerMain {
+    public static void main(String[] args) {
+
+    }
+}
