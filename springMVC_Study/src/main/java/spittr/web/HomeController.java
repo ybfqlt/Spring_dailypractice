83929ae15354a6e384aee4c039689961d0914097
ybfqlt <823030735@qq.com>
2019-04-21 15:31:15 +0800

    4-21定义类级别的请求处理

--- springMVC_Study/src/main/java/spittr/web/HomeController.java
+++ springMVC_Study/src/main/java/spittr/web/HomeController.java
@@ -5,11 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@Controller
+/*@Controller
 public class HomeController {
     @RequestMapping(value="/",method=GET)
     public String home(){
         return "home";
     }
-}
+}*/
 
+@Controller
+@RequestMapping("/")
+public class HomeController{
+    @RequestMapping(method=GET)
+    public String home(){
+        return "home";
+    }
+}
