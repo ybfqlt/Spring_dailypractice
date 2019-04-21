310d639952b29b9d393f5ffdd39bb4572509fc10
ybfqlt <823030735@qq.com>
2019-04-21 14:53:50 +0800

    4-21改进测试控制器

--- springMVC_Study/src/test/spittr/config/HomeControllerTest.java
+++ springMVC_Study/src/test/spittr/config/HomeControllerTest.java
@@ -1,14 +1,24 @@
 package spittr.config;
 
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
 import spittr.web.HomeController;
-import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
 
 public class HomeControllerTest {
-    @Test
+ /*   @Test
     public void testHomePage() throws Exception{
         HomeController controller = new HomeController();
         assertEquals("home",controller.home());
+    }*/
+    @Test
+    public void testHomePage() throws Exception{
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"));
     }
 }
 
