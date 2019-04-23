3542a5e4acac8239a83b6a57a6d0484a9334744f
ybfqlt <823030735@qq.com>
2019-04-23 22:43:19 +0800

    4-23待修改

--- springMVC_Study/src/test/spittr/config/controllernewTest.java
+++ springMVC_Study/src/test/spittr/config/controllernewTest.java
@@ -1,7 +1,6 @@
 package spittr.config;
 
 import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
